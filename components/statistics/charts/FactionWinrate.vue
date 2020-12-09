f05ca7c6126797d7fdcbc1b1db6b0055d4897e32
MaxPowerEdain <56118011+MaxPowerEdain@users.noreply.github.com>
2020-12-09 17:09:31 +0100

    removed labels plugin and added datalabel plugin

--- components/statistics/charts/FactionWinrate.vue
+++ components/statistics/charts/FactionWinrate.vue
@@ -1,10 +1,49 @@
-<template>
-  <p>Faction Winrate chart placeholder</p>
-</template>
-
 <script>
-export default {
+import { Pie } from 'vue-chartjs';
+import ChartJSPluginDatalabels from "chartjs-plugin-datalabels";
 
+export default {
+  extends: Pie,
+  data: () => ({
+    chartdata: {
+      labels: ['January', 'February'],
+      datasets: [
+        {
+          label: null,
+          backgroundColor: ['#146478', '#167389'],
+          data: [40, 20]
+        }
+      ]
+    },
+    options: {
+      legend: {
+        display: false
+      },
+      responsive: true,
+      maintainAspectRatio: false,
+      animation: {
+        animateRotate: true,
+        animateScale: true
+      },
+      tooltips: {
+        cornerRadius: 0,
+        intersect: false
+      },
+      plugins: {
+        datalabels: {
+          color: 'black',
+          anchor: 'end',
+          align: 'end',
+          visibility: 'auto',
+          //offset: 100
+        }
+      }
+    }
+  }),
+
+  mounted () {
+    this.renderChart(this.chartdata, this.options);
+  }
 }
 </script>
 
